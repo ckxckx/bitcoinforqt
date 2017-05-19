802dd2a19013cd8ceb15de31c6e850ef59565c64
ckxckx <ckx1025ckx@sina.com>
2017-05-19 19:32:41 +0800

    jjj
    kkk

---
+++ jsontohex.py
@@ -0,0 +1,148 @@
+import json
+
+# dec to byte in little-endian or big-endian
+def dec2byte(num, byte=None, format="little-endian"):
+	if byte == None:
+		tmp = "%x" %num
+		if len(tmp) % 2 == 1:
+			tmp = "0" + tmp
+	else:
+		tmp = ("%0"+"%s" % (2*byte) +"x") %num
+	if format == "little-endian":
+		tmp = big2little(tmp)
+	return tmp
+
+# format the byte data in little-endian
+def big2little(bigstr):
+	return "".join([bigstr[2*i:2*i+2] for i in xrange(len(bigstr)/2)][::-1])
+
+# transform json tx to raw tx in hex
+def json2hex(in_json):
+	# in_json must have keys showing as below: ['version', 'inputs', 'outputs', 'lock_time']
+	# inputs of in_json must have keys showing as below: ['txid_prev', 'out_prev', 'script_sig', 'squence']
+	# outputs of in_json must have keys showing as below: ['value', 'script_pub', 'adress', 'script_type']
+
+	result = []
+	# add version to hex
+	result.append(dec2byte(in_json['version'], 4))
+	# add input_num to hex
+	result.append(dec2byte(len(in_json['inputs'])))
+	# add inputs to hex
+	for _, perin in enumerate(in_json['inputs']):
+		result.append(big2little(perin['txid_prev']))
+		result.append(dec2byte(perin['out_prev'], 4))
+
+		# add script_pubkey to hex
+		result.append(dec2byte(len(perin['script_sig'])/2))
+		result.append(perin['script_sig'])
+
+
+################add by ckx#####################
+		result.append('ffffffff')
+#################################################
+		# squence can be int
+		if isinstance(perin['sequence'], int):
+			result.append(dec2byte(perin['sequence'], 4))
+		# or be normal hex-str
+		elif isinstance(perin['sequence'], str):
+			result.append(big2little(perin['sequence']))
+
+	# add output_num to hex
+	result.append(dec2byte(len(in_json['outputs'])))
+	# add output to hex
+	for _, perout in enumerate(in_json['outputs']):
+		result.append(dec2byte(perout['value'], 8))
+
+		# add script_pubkey to hex
+		result.append(dec2byte(len(perout['script_pub'])/2))
+		result.append(perout['script_pub'])
+
+	# add lock_time to hex
+	result.append(dec2byte(in_json['lock_time'], 4))
+	return "".join(result)
+
+if __name__ == '__main__':
+	# testjson is a sample of json transaction
+	# testhex is the hex string of above transaction
+	testjson = {
+		"version": 1, 
+		"lock_time": 0, 
+		"inputs": [
+			{
+				# "txid_prev": "c8ea8b221580ebb2f1cabc8b40797bffec742b97c82a329df96d93121db43519",
+				"txid_prev": "d6602dd740ffb455a84943fa38a202b669926bbbd572a801cc6d7174b2c18b30",
+
+				"out_prev": 0, 
+				"script_sig": "483045022100921fc36b911094280f07d8504a80fbab9b823a25f102e2bc69b14bcd369dfc7902200d07067d47f040e724b556e5bc3061af132d5a47bd96e901429d53c41e0f8cca012102152e2bb5b273561ece7bbe8b1df51a4c44f5ab0bc940c105045e2cc77e618044", 
+				"sequence": 4294967295
+			}, 
+		], 
+		"outputs": [
+			{
+				"value": 1000000, 
+				"script_pub": "76a9145fb1af31edd2aa5a2bbaa24f6043d6ec31f7e63288ac", 
+			}, 
+			{
+				"value": 3988000, 
+				"script_pub": "76a914efec6de6c253e657a9d5506a78ee48d89762fb3188ac", 
+			}, 
+		]
+	}
+
+	tj={
+		"version": 1,
+		# "lock_time": 0,
+
+		"vout_sz": 2,
+		"received": "2017-05-10T13:20:23.860490658Z",
+		"lock_time": 0,
+		"hash": "405fdce67c0979769d14d85cc70590c3102db8d90c92fd928b0552ddd5dcc71a",
+		"addresses": [
+		"mgN94Z7hRWy4UfZHTj2T8hZd1BctusLkps",
+		"mzPnKxCTMF3Dx9Nx9ktZ6n72LFheEX3MpQ"
+		],
+		"inputs": [
+		{
+		"addresses": [
+		"mgN94Z7hRWy4UfZHTj2T8hZd1BctusLkps"
+		],
+		"script_sig": "",
+		"sequence": 4294967295,
+		"output_value": 155798,
+		"output_index": 1,
+		"out_prev": 0,
+		# "txid_prev": "ef4351885fa3910d6654f1b4bda06033519f603460b7fa225c0659af994f4ec8",
+
+		"txid_prev": "d6602dd740ffb455a84943fa38a202b669926bbbd572a801cc6d7174b2c18b30",
+		"script_type":""
+		}
+		],
+		"outputs": [
+		{
+		"script_type": "pay-to-pubkey-hash",
+		"addresses": [
+		"mzPnKxCTMF3Dx9Nx9ktZ6n72LFheEX3MpQ"
+		],
+		"value": 1,
+		"script_pub": "76a914cf0f286c587aeef3dd0293c00dabb90c47da0a8388ac"
+		},
+		{
+		"script_type": "pay-to-pubkey-hash",
+		"addresses": [
+		"mgN94Z7hRWy4UfZHTj2T8hZd1BctusLkps"
+		],
+		"value": 112697,
+		"script_pub": "76a914094d5ec3d22b64175fcf316d6fd2940e2c4ae84688ac"
+		}
+		],	}
+
+
+
+	testhex = "01000000011935b41d12936df99d322ac8972b74ecff7b79408bbccaf1b2eb8015228beac8000000006b483045022100921fc36b911094280f07d8504a80fbab9b823a25f102e2bc69b14bcd369dfc7902200d07067d47f040e724b556e5bc3061af132d5a47bd96e901429d53c41e0f8cca012102152e2bb5b273561ece7bbe8b1df51a4c44f5ab0bc940c105045e2cc77e618044ffffffff0240420f00000000001976a9145fb1af31edd2aa5a2bbaa24f6043d6ec31f7e63288ac20da3c00000000001976a914efec6de6c253e657a9d5506a78ee48d89762fb3188ac00000000"
+	result = json2hex(testjson)
+	# result = json2hex(tj)
+	if result == testhex:
+		print "Yes. The Function Works Well."
+	else:
+		print result
+		print testhex
--- test.py
+++ test.py
@@ -1,26 +1,106 @@
-from ourtools import *
-account_in=[mypriv1,mypub1,myaddr1]
-account_out=[myaddr2]
-our_create_txt(account_in,account_out)
-inputs = [{'address':account_in[2]}, ]
-outputs = [{'address': account_out[0], 'value': 1}]
-unsigned_tx = create_unsigned_tx(inputs=inputs,\
-                                 outputs=outputs, \
-                                 coin_symbol=flag,\
-                                 api_key=mytoken)
-num=len(get_input_addresses(unsigned_tx))
+from json import *
+oo='''
+{
+"vout_sz": 2,
+"received": "2017-05-10T13:20:25.519916323Z",
+"lock_time": 0,
+"hash": "dc20cdfd1e409cce6de8def9eb3b6c0ad7411dfc055204cb1e521eed3d8869e3",
+"addresses": [
+"mgN94Z7hRWy4UfZHTj2T8hZd1BctusLkps",
+"mzPnKxCTMF3Dx9Nx9ktZ6n72LFheEX3MpQ"
+],
+"inputs": [
+{
+"addresses": [
+"mgN94Z7hRWy4UfZHTj2T8hZd1BctusLkps"
+],
+"script": "483045022100f02bd30f926528b7ffcac0da4283c7494ef79bf5a4ae9d1c91a11e2c22fb3347022056abbd4997025093a56f3403170c838f790ad9c408822395ca0a6deea1733ed1012103565b4d5d01b7ecca773a57df552ce0a2ea63ab0bbfa69fd5958de99c5e62e36e",
+"sequence": 4294967295,
+"output_value": 155798,
+"output_index": 1,
+"prev_hash": "ef4351885fa3910d6654f1b4bda06033519f603460b7fa225c0659af994f4ec8",
+"script_type": "pay-to-pubkey-hash"
+}
+],
+"outputs": [
+{
+"script_type": "pay-to-pubkey-hash",
+"addresses": [
+"mzPnKxCTMF3Dx9Nx9ktZ6n72LFheEX3MpQ"
+],
+"value": 1,
+"script": "76a914cf0f286c587aeef3dd0293c00dabb90c47da0a8388ac"
+},
+{
+"script_type": "pay-to-pubkey-hash",
+"addresses": [
+"mgN94Z7hRWy4UfZHTj2T8hZd1BctusLkps"
+],
+"value": 112697,
+"script": "76a914094d5ec3d22b64175fcf316d6fd2940e2c4ae84688ac"
+}
+],
+"relayed_by": "60.207.237.115",
+"block_height": -1,
+"block_index": -1,
+"vin_sz": 1,
+"preference": "high",
+"confirmations": 0,
+"fees": 43100,
+"version": 1,
+"double_spend": false,
+"total": 112698,
+"size": 226
+}
+'''
+oo=loads(oo)
+type(oo)
 
 
-privkey_list=[account_in[0]]*num
-pubkey_list=[account_in[1]]*num
+bb='''
+{
+  "hash": "cca7507897abc89628f450e8b1e0c6fca4ec3f7b34cccf55f3f531c659ff4d79",
+  "ver": 1,
+  "vin_sz": 1,
+  "vout_sz": 2,
+  "lock_time": 0,
+  "size": 300,
+  "in": [
+    {
+      "prev_out": {
+        "hash": "a1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d",
+        "n": 0
+      },
+      "scriptSig": "30450221009908144ca6539e09512b9295c8a27050d478fbb96f8addbc3d075544dc41328702201aa528be2b907d316d2da068dd9eb1e23243d97e444d59290d2fddf25269ee0e01 042e930f39ba62c6534ee98ed20ca98959d34aa9e057cda01cfd422c6bab3667b76426529382c23f42b9b08d7832d4fee1d6b437a8526e59667ce9c4e9dcebcabb"
+    }
+  ],
+  "out": [
+    {
+      "value": "5777.00000000",
+      "scriptPubKey": "OP_DUP OP_HASH160 df1bd49a6c9e34dfa8631f2c54cf39986027501b OP_EQUALVERIFY OP_CHECKSIG",
+      "address": "1MLh2UVHgonJY4ZtsakoXtkcXDJ2EPU6RY",
+      "next_in": {
+        "hash": "3b8328fe7e53a8162cf023738a53c85a3cbf21efe517ab878e8cfecc3a2e22db",
+        "n": 0
+      }
+    },
+    {
+      "value": "4223.00000000",
+      "scriptPubKey": "04cd5e9726e6afeae357b1806be25a4c3d3811775835d235417ea746b7db9eeab33cf01674b944c64561ce3388fa1abd0fa88b06c44ce81e2234aa70fe578d455d OP_CHECKSIG",
+      "next_in": {
+        "hash": "9e744590d196b63d02a1dd7ef596fd6082286f84295d66da411a9ffebfdd1957",
+        "n": 10
+      }
+    }
+  ],
+  "nid": "1aed0e7cd223f973090d55da33a574312707d2cc4a4f4a5c70d6bbdc88b14cd1",
+  "block": "0000000013ab9f8ed78b254a429d3d5ad52905362e01bf6c682940337721eb51",
+  "blocknumber": 57044,
+  "time": "2010-05-22 18:26:08"
+}
+'''
 
-tx_signatures = make_tx_signatures(txs_to_sign=unsigned_tx['tosign'],\
-                                       privkey_list=privkey_list,\
-                                       pubkey_list=pubkey_list)
-  #  print '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-  #  print tx_signatures
-kk=broadcast_signed_transaction(unsigned_tx=unsigned_tx, \
-                                    signatures=tx_signatures,\
-                                    pubkeys=pubkey_list,\
-                                    coin_symbol=flag,\
-                                    api_key=mytoken)
+
+qqq='''
+01000000016587ed26682ba0991643c572a74d57525aa259aefae3d68475e12ef9befc98b6010000006b483045022100f64d100c1d33666dfb149fab0b066ed9047c04645821154c176f49f6ae818bfa02200a6f59346da02de6de0ba510672eb4606ba36a2a39fffe60f4e4c7e26983564301210316cff587a01a2736d5e12e53551b18d73780b83c3bfb4fcf209c869b11b6415effffffff02801a0600000000001976a914094d5ec3d22b64175fcf316d6fd2940e2c4ae84688ace5236541000000001976a914c01a7ca16b47be50cbdbc60724f701d52d75156688ac00000000
+'''
